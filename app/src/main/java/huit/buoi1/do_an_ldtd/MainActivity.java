134d85bf3b76b5133030e21bcc458c093ef1778c
PHAP <106916730+phapscac@users.noreply.github.com>
2024-05-20 14:03:36 +0700

    Update MainActivity.java
    phap sua

--- app/src/main/java/huit/buoi1/do_an_ldtd/MainActivity.java
+++ app/src/main/java/huit/buoi1/do_an_ldtd/MainActivity.java
@@ -8,9 +8,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         // Đầu tiên, lấy ảnh từ drawable bằng ID của nó
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.tai);
          tx1 = findViewById(R.id.tx1);
+        tx1.setText("12345");
 
         callAPI(getByteArrayFromBitmap(bitmap));
     }
